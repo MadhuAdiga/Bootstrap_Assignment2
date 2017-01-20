d866f477a60db6d1bd936850b9f1536bcb071ca3
MadhuAdiga <madhu.vah@gmail.com>
2017-01-20 18:46:54 +0530

    Assignment2

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+$('#modal_button').click(function(){
+$('#reg_modal').show();
+    return false;
+});
+});
+
+$(document).ready(function(){
+$('.modal-header>button').click(function(){
+$('#reg_modal').hide();
+    return false;
+});
+});
+
+  
+  
